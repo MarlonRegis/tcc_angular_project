3c104394b7247cb3ae64fa593a891bfe07916aca
Marlon Regis <marlonregis14@outlook.com>
2022-05-15 16:22:59 -0300

    correcao environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  contentful:{
+    spaceId: 'oceyxd3hvxd2',
+    token: 'icsgmQDqpfiUJBCDrzJPnvGEIGs_5b5RPoWpLddTrLo'
+  },
+  recaptcha: {
+    siteKey: '6LfKNi0cAAAAACeYwFRY9_d_qjGhpiwYUo5gNW5-',
+  }
 };
